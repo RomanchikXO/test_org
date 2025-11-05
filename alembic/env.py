3dabb3675539de803d28125fbf0a7c4b9177c528
Роман Дикарев <super.romanchik228@yandex.ru>
2025-11-05 16:27:55 +0300

    - убрал уязвимые данные из настройки alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,7 @@
+import os
 from logging.config import fileConfig
 from models import Base
+from dotenv import load_dotenv
 
 
 from sqlalchemy import engine_from_config
@@ -7,9 +9,13 @@ from sqlalchemy import pool
 
 from alembic import context
 
+load_dotenv()
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option("sqlalchemy.url", f"sqlite:///{os.getenv('DB_NAME')}")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
